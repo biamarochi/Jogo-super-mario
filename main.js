64c1e5fc2ea3e2aa44d4ae6b09739858b382fda6
biamarochi <163007667+biamarochi@users.noreply.github.com>
2024-09-16 11:13:32 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-
 const mario = document.querySelector(".mario");
 const pipe = document.querySelector(".pipe");
 const nuvem = document.querySelector(".nuvem");
 const startButton = document.querySelector(".start");
 const gameOverScreen = document.querySelector(".game-over");
 
-AudioStart = Audio("./sound/audio_theme.mp3");
+audioStart = new Audio("./sound/audio_theme.mp3");
 const gameOverSound = new Audio("./sound/audio.gameover.mp3");
 
 let gameStarted = false;
-const gameStarted = () => {
+
+const startGame = () => {
 gameStarted = true;
 audioStart.play();
 
@@ -44,7 +44,7 @@ if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
     mario.style.animation = "none";
     mario.style.left = `${marioPosition}px`;
 
-    mario.src = "game-over.png";
+    mario.src = ". /img/game-over.png";
     mario.style.width = "75px";
     mario.style.marginLeft = "50px";
     audioStart.pause();
